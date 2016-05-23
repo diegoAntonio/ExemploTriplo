8bedffa11d402c59325c7a04bca8ac2eed844502
Andre Alcantara <andre.alcantara.nti@ufpe.br>
2016-05-23 17:33:19 -0300

    Alterando as paths

--- src/main/java/br/ufpe/exemploprojeto/security/SecurityConfig.java
+++ src/main/java/br/ufpe/exemploprojeto/security/SecurityConfig.java
@@ -22,33 +22,70 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.exceptionHandling()
-				.and()
-			.authorizeRequests()
-				.antMatchers("/resource/**")
-				.permitAll().anyRequest().authenticated()
-				.and()
-			.authorizeRequests()
-				.and()  
-			.exceptionHandling()
-				.accessDeniedPage("/error-404.xhtml")
-				.and()
-			.authorizeRequests()
+		HttpSecurity config = http;
+		config = configurarResources(config);
+		config = configurarResourcesHandling(config);
+		config = configurarFormularioLogin(config);
+		config.authorizeRequests()
 				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.failureUrl("/login.xhtml").loginPage("/login.xhtml").permitAll()
-				.and()
-			.logout()
-				.logoutSuccessUrl("/login.xhtml").permitAll()
-//				.and()
-//			.formLogin().permitAll()
-			;
+				.and();
 	}
 	
-	private void configurarResources(HttpSecurity http) throws Exception {
-		
+	
+	/**
+	 * Metodo que define o formulario de login<br />
+	 * <strong> .formLogin</strong> Altera o fluxo para modificar os parametros do formulario de login.<br />
+	 * <strong> .usernameParameter</strong> Define o name do input.<br />
+	 * <strong> .passwordParameter</strong> Define o password do input.<br />
+	 * <strong> .defaultSuccessUrl</strong> Define o url padrao apos sucesso.<br />
+	 * <strong> .logoutSuccessUrl</strong> Define o url padrao apos logout com sucesso.<br />
+	 * <strong> .failureUrl</strong> Define a pagina do em caso de falha do login.<br />
+	 * <strong> .loginPage</strong> Define a pagina de login de acesso.<br /><br />
+	 * <strong> .logout</strong> Altera para o fluxo de logout.<br />
+	 * <strong> .invalidateHttpSession</strong> Define a invalidade da sessao.<br />
+	 * <strong> .logoutSuccessUrl</strong> Define a pagina de sucesso do logout.<br />
+	 * @param http - {@link HttpSecurity} eh o objeto que sera mapeado as rotas.
+	 * @return {@link HttpSecurity} retorno atualizado.
+	 * @throws Exception - Qualquer error durante as autorizacoes.
+	 */
+	private HttpSecurity configurarFormularioLogin(HttpSecurity http) throws Exception{
+		return http
+				.formLogin().defaultSuccessUrl("/index.xhtml").usernameParameter("username")
+				.failureUrl("/login.xhtml").loginPage("/login.xhtml").passwordParameter("password")
+				.permitAll().and()
+				.logout().invalidateHttpSession(true).logoutSuccessUrl("/login.xhtml").permitAll().and();
+	}
+	
+	/**
+	 * Metodo que define os recursos utilizado nas paginas.
+	 * 
+	 * @param http - {@link HttpSecurity} eh o objeto que sera mapeado as rotas.
+	 * @return {@link HttpSecurity} retorno atualizado.
+	 * @throws Exception - Qualquer error durante as autorizacoes.
+	 */
+	private HttpSecurity configurarResources(HttpSecurity http) throws Exception {
+		return  http
+//	            .antMatcher("/**")
+////	            .authorizeRequests()
+////	            .anyRequest()
+//	            .access("authenticated").and()
+//				.permitAll().anyRequest().authenticated()
+//				.and()
+				.authorizeRequests().antMatchers("/javax.faces.resource/**").permitAll()
+				.and()
+				.authorizeRequests().antMatchers("/resource/**").permitAll()
+				.and();
+	}
+	
+	/**
+	 * @param http - {@link HttpSecurity} eh o objeto que sera mapeado as rotas.
+	 * @return {@link HttpSecurity} retorno atualizado.
+	 * @throws Exception - Qualquer error durante as autorizacoes.
+	 */
+	private HttpSecurity configurarResourcesHandling(HttpSecurity http) throws Exception {
+		return http
+				.exceptionHandling().accessDeniedPage("/error-404.xhtml")
+				.and();
 	}
 	
 }	
